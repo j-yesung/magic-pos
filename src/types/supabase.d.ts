7d883062148e8b1fb68d55b2d8f31442be129292
sanghyeon <uphyeon@kakao.com>
2024-01-08 13:00:47 +0900

    :wrench: setting: supabase 설정 파일 및 타입 생성 (#9)

---
+++ src/types/supabase.d.ts
@@ -0,0 +1,449 @@
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+
+export interface Database {
+  public: {
+    Tables: {
+      cart: {
+        Row: {
+          id: string;
+          is_togo: boolean | null;
+          menu_list: Json[] | null;
+          order_id: string | null;
+          order_number: number | null;
+          order_time: string | null;
+          payment_method_id: number | null;
+          store_id: string;
+          total_fee: number | null;
+        };
+        Insert: {
+          id?: string;
+          is_togo?: boolean | null;
+          menu_list?: Json[] | null;
+          order_id?: string | null;
+          order_number?: number | null;
+          order_time?: string | null;
+          payment_method_id?: number | null;
+          store_id: string;
+          total_fee?: number | null;
+        };
+        Update: {
+          id?: string;
+          is_togo?: boolean | null;
+          menu_list?: Json[] | null;
+          order_id?: string | null;
+          order_number?: number | null;
+          order_time?: string | null;
+          payment_method_id?: number | null;
+          store_id?: string;
+          total_fee?: number | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'cart_payment_method_id_fkey';
+            columns: ['payment_method_id'];
+            isOneToOne: false;
+            referencedRelation: 'payment_method';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'cart_store_id_fkey';
+            columns: ['store_id'];
+            isOneToOne: false;
+            referencedRelation: 'store';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      distribution: {
+        Row: {
+          company_name: string | null;
+          id: string;
+          menu_id: string;
+        };
+        Insert: {
+          company_name?: string | null;
+          id?: string;
+          menu_id: string;
+        };
+        Update: {
+          company_name?: string | null;
+          id?: string;
+          menu_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'distribution_menu_id_fkey';
+            columns: ['menu_id'];
+            isOneToOne: false;
+            referencedRelation: 'menu_item';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      distribution_detail: {
+        Row: {
+          distribution_id: string;
+          id: string;
+          min_ea: number | null;
+          name: string | null;
+          price: number | null;
+        };
+        Insert: {
+          distribution_id: string;
+          id?: string;
+          min_ea?: number | null;
+          name?: string | null;
+          price?: number | null;
+        };
+        Update: {
+          distribution_id?: string;
+          id?: string;
+          min_ea?: number | null;
+          name?: string | null;
+          price?: number | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'distribution_detail_distribution_id_fkey';
+            columns: ['distribution_id'];
+            isOneToOne: false;
+            referencedRelation: 'distribution';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      menu_category: {
+        Row: {
+          id: string;
+          name: string | null;
+          position: number | null;
+          store_id: string;
+        };
+        Insert: {
+          id?: string;
+          name?: string | null;
+          position?: number | null;
+          store_id: string;
+        };
+        Update: {
+          id?: string;
+          name?: string | null;
+          position?: number | null;
+          store_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'menu_category_store_id_fkey';
+            columns: ['store_id'];
+            isOneToOne: false;
+            referencedRelation: 'store';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      menu_item: {
+        Row: {
+          category_id: string;
+          id: string;
+          image_url: string | null;
+          name: string | null;
+          remain_ea: number | null;
+        };
+        Insert: {
+          category_id: string;
+          id?: string;
+          image_url?: string | null;
+          name?: string | null;
+          remain_ea?: number | null;
+        };
+        Update: {
+          category_id?: string;
+          id?: string;
+          image_url?: string | null;
+          name?: string | null;
+          remain_ea?: number | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'menu_item_category_id_fkey';
+            columns: ['category_id'];
+            isOneToOne: false;
+            referencedRelation: 'menu_category';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      menu_option: {
+        Row: {
+          id: string;
+          is_use: boolean | null;
+          menu_id: string;
+          name: string | null;
+          option_kind: string[] | null;
+        };
+        Insert: {
+          id?: string;
+          is_use?: boolean | null;
+          menu_id: string;
+          name?: string | null;
+          option_kind?: string[] | null;
+        };
+        Update: {
+          id?: string;
+          is_use?: boolean | null;
+          menu_id?: string;
+          name?: string | null;
+          option_kind?: string[] | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'menu_option_menu_id_fkey';
+            columns: ['menu_id'];
+            isOneToOne: false;
+            referencedRelation: 'menu_item';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      payment_method: {
+        Row: {
+          id: number;
+          name: string;
+        };
+        Insert: {
+          id?: number;
+          name: string;
+        };
+        Update: {
+          id?: number;
+          name?: string;
+        };
+        Relationships: [];
+      };
+      profiles: {
+        Row: {
+          created_at: string;
+          id: string;
+          is_disabled: boolean | null;
+          name: string | null;
+        };
+        Insert: {
+          created_at?: string;
+          id: string;
+          is_disabled?: boolean | null;
+          name?: string | null;
+        };
+        Update: {
+          created_at?: string;
+          id?: string;
+          is_disabled?: boolean | null;
+          name?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'profiles_id_fkey';
+            columns: ['id'];
+            isOneToOne: true;
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      sales: {
+        Row: {
+          id: string;
+          product_category: string | null;
+          product_ea: number | null;
+          product_name: string | null;
+          product_price: number | null;
+          sales_date: string | null;
+          store_id: string;
+        };
+        Insert: {
+          id?: string;
+          product_category?: string | null;
+          product_ea?: number | null;
+          product_name?: string | null;
+          product_price?: number | null;
+          sales_date?: string | null;
+          store_id: string;
+        };
+        Update: {
+          id?: string;
+          product_category?: string | null;
+          product_ea?: number | null;
+          product_name?: string | null;
+          product_price?: number | null;
+          sales_date?: string | null;
+          store_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'sales_store_id_fkey';
+            columns: ['store_id'];
+            isOneToOne: false;
+            referencedRelation: 'store';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      store: {
+        Row: {
+          business_id: string;
+          business_name: string | null;
+          business_number: string | null;
+          end_time: string | null;
+          id: string;
+          order_number: number | null;
+          start_time: string | null;
+        };
+        Insert: {
+          business_id: string;
+          business_name?: string | null;
+          business_number?: string | null;
+          end_time?: string | null;
+          id?: string;
+          order_number?: number | null;
+          start_time?: string | null;
+        };
+        Update: {
+          business_id?: string;
+          business_name?: string | null;
+          business_number?: string | null;
+          end_time?: string | null;
+          id?: string;
+          order_number?: number | null;
+          start_time?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'store_business_id_fkey';
+            columns: ['business_id'];
+            isOneToOne: false;
+            referencedRelation: 'profiles';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      store_table: {
+        Row: {
+          id: string;
+          is_disabled: boolean | null;
+          max_guest: number | null;
+          name: string | null;
+          position: number | null;
+          store_id: string;
+        };
+        Insert: {
+          id?: string;
+          is_disabled?: boolean | null;
+          max_guest?: number | null;
+          name?: string | null;
+          position?: number | null;
+          store_id: string;
+        };
+        Update: {
+          id?: string;
+          is_disabled?: boolean | null;
+          max_guest?: number | null;
+          name?: string | null;
+          position?: number | null;
+          store_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'store_table_store_id_fkey';
+            columns: ['store_id'];
+            isOneToOne: false;
+            referencedRelation: 'store';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+}
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database['public']['Tables'] & Database['public']['Views'])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+        Database[PublicTableNameOrOptions['schema']]['Views'])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database['public']['Tables'] & Database['public']['Views'])
+    ? (Database['public']['Tables'] & Database['public']['Views'])[PublicTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends keyof Database['public']['Tables'] | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+    ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends keyof Database['public']['Tables'] | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+    ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  PublicEnumNameOrOptions extends keyof Database['public']['Enums'] | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database['public']['Enums']
+    ? Database['public']['Enums'][PublicEnumNameOrOptions]
+    : never;
