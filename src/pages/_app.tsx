6425d0fea22ccf3ba288e51777f8fc1235b81c90
sanghyeon <uphyeon@kakao.com>
2024-01-09 00:23:55 +0900

    🩹 fix: merge conflict (#5)
    _app.tsx conflict 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { NextPageWithLayout } from '@/types/common';
+
+const queryClient = new QueryClient();
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? (page => page);
-
-  return getLayout(<Component {...pageProps} />);
+  return <QueryClientProvider client={queryClient}>{getLayout(<Component {...pageProps} />)}</QueryClientProvider>;
 }
