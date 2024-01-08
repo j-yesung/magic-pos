0ea46480669d18655d9f4fa12de6055a98eb65fb
sanghyeon <uphyeon@kakao.com>
2024-01-08 17:54:38 +0900

    :bulb: comment: 주석 추가 (#5)

---
+++ src/shared/store/order.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand';
+
+/**
+ * 일반고객의 주문과 관련된 전역 상태를 관리하는 store 입니다.
+ */
+
+interface OrderState {
+  step: number;
+  readonly maxStep: number;
+  goNextStep: () => void;
+  goPrevStep: () => void;
+}
+
+const useAuthStore = create<OrderState>()(set => ({
+  step: 0,
+  maxStep: 4,
+  goNextStep: () => set(state => ({ step: Math.min(state.step + 1, state.maxStep) })),
+  goPrevStep: () => set(state => ({ step: Math.max(state.step - 1, 0) })),
+}));
+
+export default useAuthStore;
