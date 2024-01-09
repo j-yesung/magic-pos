90c0179444fcb18b0daf811f1c1c0a768c4d7849
LeeDongTak <sunny5796@naver.com>
2024-01-09 22:29:16 +0900

    ✨ update: _app.tsx에 react-query-devtool 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import '@/styles/reset.css';
 import { NextPageWithLayout } from '@/types/common';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 
 const queryClient = new QueryClient();
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? (page => page);
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       {getLayout(<Component {...pageProps} />)}
       <Modal />
     </QueryClientProvider>
