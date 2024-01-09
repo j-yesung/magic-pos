3c52d24e92a1d72ca20555a83c2694ca751294b0
lunaxislu <cmk0905@naver.com>
2024-01-09 22:34:19 +0900

    🔥 del : content 폴더 및 파일 아직 필요 없어서 삭제함

--- src/components/sales/status/Status.tsx
+++ src/components/sales/status/Status.tsx
@@ -1,11 +1,9 @@
-import Content from './content/Content';
 import Tab from './tab/Tab';
 
 const Status = () => {
   return (
     <div>
       <Tab />
-      <Content />
     </div>
   );
 };
--- src/components/sales/status/content/Content.tsx
+++
@@ -1,5 +0,0 @@
-const Content = () => {
-  return <div></div>;
-};
-
-export default Content;
