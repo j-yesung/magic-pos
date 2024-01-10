137148052ed13435528d45dfe890ee0e00c993b9
j-yesung <hisung-ah@kakao.com>
2024-01-10 23:38:42 +0900

    🩹 fix: 사이드바 Link로 수정 (추후 경로 수정)

--- src/components/layout/admin/nav/NavBar.tsx
+++ src/components/layout/admin/nav/NavBar.tsx
@@ -1,14 +1,15 @@
+import Link from 'next/link';
 import { useState } from 'react';
 import styles from '../styles/AdminLayout.module.css';
 
 const NavBar = () => {
   const [navList, setNavList] = useState([
-    { id: 1, name: '주문 내역 확인', active: false },
-    { id: 2, name: '매출 관리', active: false },
-    { id: 3, name: '플랫폼 관리', active: false },
-    { id: 4, name: '테이블 관리', active: true },
-    { id: 5, name: '메뉴 관리', active: false },
-    { id: 6, name: '가게 설정', active: false },
+    { id: 1, name: '주문 내역 확인', url: '#1', active: false },
+    { id: 2, name: '매출 관리', url: '#2', active: false },
+    { id: 3, name: '플랫폼 관리', url: '#3', active: false },
+    { id: 4, name: '테이블 관리', url: '#4', active: true },
+    { id: 5, name: '메뉴 관리', url: '#5', active: false },
+    { id: 6, name: '가게 설정', url: '/admin/store', active: false },
   ]);
 
   const clickNavListHandler = (id: number) => {
@@ -22,7 +23,7 @@ const NavBar = () => {
       <ul>
         {navList.map(list => (
           <li className={list.active ? styles.active : ''} key={list.id} onClick={() => clickNavListHandler(list.id)}>
-            <span>{list.name}</span>
+            <Link href={list.url}>{list.name}</Link>
           </li>
         ))}
       </ul>
