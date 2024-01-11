446472869c36d16be75e291ae09d108988898033
j-yesung <hisung-ah@kakao.com>
2024-01-11 23:57:10 +0900

    🩹 fix: auth 페이지 title & meta tag 추가

--- src/pages/auth/reset.tsx
+++ src/pages/auth/reset.tsx
@@ -1,5 +1,16 @@
 import User from '@/components/auth/User';
+import Head from 'next/head';
 
-const ResetPage = () => <User />;
+const ResetPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Magic Pos</title>
+        <meta name="description" content="password reset" />
+      </Head>
+      <User />
+    </>
+  );
+};
 
 export default ResetPage;
