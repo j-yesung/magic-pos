ee5b6c5dc2bbf242b5394ebb75ad2714e4af12d2
j-yesung <hisung-ah@kakao.com>
2024-01-11 23:53:07 +0900

    🩹 fix: HomePage - add title & meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import HomeLayout from '@/components/layout/home/HomeLayout';
+import Head from 'next/head';
 import { ReactNode } from 'react';
 
 const HomePage = () => {
-  return <></>;
+  return (
+    <Head>
+      <title>Magic Pos</title>
+      <meta name="description" content="magic pos in the palm of your hand" />
+    </Head>
+  );
 };
 
 HomePage.getLayout = (page: ReactNode) => <HomeLayout>{page}</HomeLayout>;
