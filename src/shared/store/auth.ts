d6ca5487bebf80536076f1c99e37168aec093102
j-yesung <hisung-ah@kakao.com>
2024-01-12 10:52:37 +0900

    🩹 fix: 세션 저장 key 이름 수정

--- src/shared/store/auth.ts
+++ src/shared/store/auth.ts
@@ -2,16 +2,13 @@ import { AuthState } from '@/types/common';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-// 예시 store 입니다.
-
 const useAuthStore = create<AuthState>()(
-  // setSession: session => set({ session }),
   persist(
     set => ({
       session: null,
       setSession: session => set({ session }),
     }),
-    { name: 'user-StoreName' },
+    { name: 'session-status' },
   ),
 );
 
