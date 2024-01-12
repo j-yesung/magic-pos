27739d4bd0ffdd4ad7640447c1377bad34612518
j-yesung <hisung-ah@kakao.com>
2024-01-13 00:12:30 +0900

    🚚 move: 비디오 섹션 생성

--- src/components/layout/home/main/Contents.tsx
+++ src/components/layout/home/main/Contents.tsx
@@ -9,20 +9,7 @@ const Contents = () => {
       <WateMark />
       <StartButton />
       <SliderArea />
-
-      <section className={styles.videoWrapper}>
-        <div className={styles.videoTextArea}>
-          <h1>번거로운 키오스크는 이제 안녕! 👋</h1>
-          <div>
-            <p>매직 포스에서 메뉴 선택부터 주문까지</p>
-            <p>그리고 점주분들은 매장 관리까지! 🖥️</p>
-          </div>
-        </div>
-
-        <div className={styles.videoSection}>
-          <div>비디오</div>
-        </div>
-      </section>
+      <Video />
     </>
   );
 };
