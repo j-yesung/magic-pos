058b5aeea7ecff9a564d7c1c5695f1d2d148cc06
j-yesung <hisung-ah@kakao.com>
2024-01-13 00:11:19 +0900

    🚚 move: 파일 export home 경로로 이동

---
+++ src/components/layout/home/module-export.ts
@@ -0,0 +1,8 @@
+import SliderArea from './main/Slider';
+import StartButton from './main/StartButton';
+import StickBar from './main/StickBar';
+import Title from './main/Title';
+import WateMark from './main/WateMark';
+import Analysis from './main/section/Analysis';
+import Video from './main/section/Video';
+export { Analysis, SliderArea, StartButton, StickBar, Title, Video, WateMark };
