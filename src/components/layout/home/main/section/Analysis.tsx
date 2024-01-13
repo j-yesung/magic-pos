55894da0ec9f0c707d6f516901ab87792ffc9d60
j-yesung <hisung-ah@kakao.com>
2024-01-13 23:40:25 +0900

    🌱 add: 매출 분석 섹션 추가

--- src/components/layout/home/main/section/Analysis.tsx
+++ src/components/layout/home/main/section/Analysis.tsx
@@ -1,8 +1,27 @@
+import styles from '../../styles/Section.module.css';
+import OtherFeature from './OtherFeature';
+
 const Analysis = () => {
   return (
-    <section>
-      <div></div>
-    </section>
+    <>
+      <section className={styles.salesWrapper}>
+        <div className={styles.salesTextArea}>
+          <h1>꼼꼼한 매출 분석까지 도와드려요.</h1>
+          <span>우리 가게는 언제 가장 매출이 높았는지 플랜을 세워보세요.</span>
+        </div>
+
+        <div className={styles.salsePictrueArea}>
+          <div className={styles.salesContentsContainer}>
+            <h1>매출 분석</h1>
+            <OtherFeature />
+          </div>
+          <div className={styles.salesContentsContainer}>
+            <h1>다른 기능</h1>
+            <OtherFeature />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
