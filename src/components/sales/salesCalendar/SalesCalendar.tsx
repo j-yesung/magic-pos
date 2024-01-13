9acc5cf76e338b653ae85a805a0840e1f49ea6ab
lunaxislu <cmk0905@naver.com>
2024-01-13 15:07:42 +0900

    🍻 test :getMonthSales 함수 되는 확인 완료

--- src/components/sales/salesCalendar/SalesCalendar.tsx
+++ src/components/sales/salesCalendar/SalesCalendar.tsx
@@ -1,5 +1,23 @@
+import { getMonthSales } from '@/server/api/supabase/sales';
+import useManagementState from '@/shared/store/management';
+import { useEffect } from 'react';
+import Header from '../status/tab/calendar/header/Header';
+
 const SalesCalendar = () => {
-  return <div></div>;
+  const {
+    date: { currentDate },
+  } = useManagementState();
+  useEffect(() => {
+    getMonthSales(currentDate.clone()).then(data => {
+      console.log(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default SalesCalendar;
