48c6c8456a65398eba4e85d806c956d7bee476ae
LeeDongTak <sunny5796@naver.com>
2024-01-13 21:57:08 +0900

    🎉 update: 포장주문 리스트에서 is_toge에 true만 갖고오기

--- src/components/management/managementContainer/PackagingContainer.tsx
+++ src/components/management/managementContainer/PackagingContainer.tsx
@@ -9,16 +9,17 @@ const PackagingContainer = ({ managementData }: { managementData?: StoreWithOrde
       <div className={styles['packaging-title']}>포장 주문</div>
       <ul className={styles['packaging-list']}>
         {
-          managementData?.[0]?.order_number.sort((a, b) => {
-            if (a.order_time && b.order_time) {
-              return a.order_time > b.order_time ? -1 : 1
-            }
-            return 0
-          }).map((item) => {
-            return (
-              <PackagingListItem key={item.id} packagingData={item} />
-            )
-          })
+          managementData?.[0]?.order_number.filter((item) => item.is_togo === true)
+            .sort((a, b) => {
+              if (a.order_time && b.order_time) {
+                return a.order_time > b.order_time ? -1 : 1
+              }
+              return 0
+            }).map((item) => {
+              return (
+                <PackagingListItem key={item.id} packagingData={item} />
+              )
+            })
         }
       </ul>
     </div>
