18e441f0265854bb51ae561f86bf02989793a543
j-yesung <hisung-ah@kakao.com>
2024-01-13 23:39:58 +0900

    🌱 add: 다른 기능 세션 페이지 추가 (swiper 활용)

---
+++ src/components/layout/home/main/section/OtherFeature.tsx
@@ -0,0 +1,38 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import styles from '../../styles/Section.module.css';
+
+const swiperDummy = [
+  { id: 1, title: '첫 번째 이미지' },
+  { id: 2, title: '두 번째 이미지' },
+  { id: 3, title: '세 번째 이미지' },
+];
+
+const OtherFeature = () => {
+  return (
+    <>
+      <div className={styles.salesContents}>
+        <Swiper
+          className={styles.swiperWrapper}
+          modules={[Navigation, Pagination]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation={true}
+          pagination={{ clickable: true }}
+          loop={true}
+        >
+          {swiperDummy.map(item => (
+            <SwiperSlide className={styles.sliderItem} key={item.id}>
+              <h1>{item.title}</h1>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default OtherFeature;
