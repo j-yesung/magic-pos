d61cea7b35b172179e26e8a348085c54c670b5b2
j-yesung <hisung-ah@kakao.com>
2024-01-14 21:57:52 +0900

    fix: 사이드바 카테고리 클릭 시 active 상태는 변경되지 않도록 (#35)

--- src/components/layout/admin/nav/NavBar.tsx
+++ src/components/layout/admin/nav/NavBar.tsx
@@ -7,6 +7,10 @@ const NavBar = (adminInfo: AdminCategories) => {
   const router = useRouter();
 
   const clickNavListHandler = (id: number, url: string) => {
+    // 현재 active 상태인 list는 클릭해도 반응하지 않습니다.
+    const currentActive = navList.find(item => item.active === true);
+    if (currentActive && currentActive.id === id) return;
+
     setNavList(prevList =>
       prevList.map(item => (item.id === id ? { ...item, active: !item.active } : { ...item, active: false })),
     );
@@ -18,8 +22,12 @@ const NavBar = (adminInfo: AdminCategories) => {
     <aside className={styles.navWrapper}>
       <ul>
         {navList.map(list => (
-          <li className={list.active ? styles.active : ''} key={list.id}>
-            <button onClick={() => clickNavListHandler(list.id, list.url)}>{list.name}</button>
+          <li
+            className={list.active ? styles.active : ''}
+            key={list.id}
+            onClick={() => clickNavListHandler(list.id, list.url)}
+          >
+            {list.name}
           </li>
         ))}
       </ul>
