324fac24a3eed151376ea777f755d40ee3953d34
j-yesung <hisung-ah@kakao.com>
2024-01-14 21:32:38 +0900

    🩹 fix: 카테고리 클릭 이벤트 버블링 수정

--- src/components/layout/admin/nav/NavBar.tsx
+++ src/components/layout/admin/nav/NavBar.tsx
@@ -1,22 +1,25 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import styles from '../styles/AdminLayout.module.css';
 
 const NavBar = (adminInfo: AdminCategories) => {
   const [navList, setNavList] = useState(adminInfo.adminCategories);
+  const router = useRouter();
 
-  const clickNavListHandler = (id: number) => {
+  const clickNavListHandler = (id: number, url: string) => {
     setNavList(prevList =>
       prevList.map(item => (item.id === id ? { ...item, active: !item.active } : { ...item, active: false })),
     );
+
+    router.push(url);
   };
 
   return (
     <aside className={styles.navWrapper}>
       <ul>
         {navList.map(list => (
-          <li className={list.active ? styles.active : ''} key={list.id} onClick={() => clickNavListHandler(list.id)}>
-            <Link href={list.url}>{list.name}</Link>
+          <li className={list.active ? styles.active : ''} key={list.id}>
+            <button onClick={() => clickNavListHandler(list.id, list.url)}>{list.name}</button>
           </li>
         ))}
       </ul>
