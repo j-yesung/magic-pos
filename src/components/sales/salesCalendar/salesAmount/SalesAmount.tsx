b0d444f552af6f3cbb3b7d93ac3e3f9797f29f2f
lunaxislu <cmk0905@naver.com>
2024-01-14 16:57:01 +0900

    💄 기본 layout 잡음

--- src/components/sales/salesCalendar/salesAmount/SalesAmount.tsx
+++ src/components/sales/salesCalendar/salesAmount/SalesAmount.tsx
@@ -1,5 +1,16 @@
+import useSalesStore from '@/shared/store/sales';
+import styles from './styles/salesAmount.module.css';
 const SalesAmount = () => {
-  return <div>ㅁㄴㅇㄹ</div>;
+  const { salesSum } = useSalesStore();
+  return (
+    <div className={styles.container}>
+      <div>매출금액</div>
+      <div className={styles.sumSales}>
+        <span> 총 매출금액 </span>
+        <span>{salesSum}</span>
+      </div>
+    </div>
+  );
 };
 
 export default SalesAmount;
