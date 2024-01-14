e40d11588b11bb8a5c251047f24a28428f0722ee
j-yesung <hisung-ah@kakao.com>
2024-01-14 23:51:07 +0900

    🩹 fix: 로그인 안 했을 때, 어드민 페이지 접근 시 홈으로 이동

--- src/components/layout/admin/AdminLayout.tsx
+++ src/components/layout/admin/AdminLayout.tsx
@@ -19,6 +19,12 @@ const adminCategories = [
 
 const AdminLayout = ({ children }: AdminLayoutProps) => {
   const { isToggle } = useToggleStore();
+  const { auth } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!auth) router.push('/');
+  }, []);
 
   return (
     <div className={isToggle ? styles.adminWrapperToggle : styles.adminWrapper}>
