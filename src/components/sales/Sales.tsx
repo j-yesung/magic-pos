69e0ae71327c5c6ccebab841ca3c0cfea1aa1fdd
lunaxislu <cmk0905@naver.com>
2024-01-15 14:57:06 +0900

    ✏️ toggle 함수 기능 명칭, 및 기능 세분화

--- src/components/sales/Sales.tsx
+++ src/components/sales/Sales.tsx
@@ -4,16 +4,18 @@ import Status from './status/Status';
 import styles from './styles/sales.module.css';
 const Sales = () => {
   const [toggle, setToggle] = useState(false);
-  const clickChangeSales = () => setToggle(pre => !pre);
+  const clickShowChart = () => setToggle(pre => true);
+  const clickShowCalendar = () => setToggle(pre => false);
   return (
     <div>
       <div className={styles.titleWrapper}>
         <h2>매출관리</h2>
         <div>
-          <button onClick={clickChangeSales}>매출관리</button>
-          <button onClick={clickChangeSales}>매출달력</button>
+          <button onClick={clickShowChart}>매출관리</button>
+          <button onClick={clickShowCalendar}>매출달력</button>
         </div>
       </div>
+
       {toggle ? <SalesDeatilWithCalendar /> : <Status />}
     </div>
   );
