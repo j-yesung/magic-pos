5f3a8595f00b6f181be9bd26dd253e4db7316c53
hojun.yoon <tjdsksro@gmail.com>
2024-01-17 18:01:42 +0900

    💄 style: swiper 좌우 그라이데션 css 추가 및 정리 (#26)

--- src/components/menu-item/MenuItemCategory.tsx
+++ src/components/menu-item/MenuItemCategory.tsx
@@ -6,10 +6,12 @@ import 'swiper/css';
 import { Virtual } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './styles/menu-item-category.module.css';
+import { useState } from 'react';
 
 const MenuItemCategoryPage = () => {
   const { setMenuItemList, categoryWithMenuItem, setCategoryWithMenuItem, categoryWithMenuItemList } =
     useMenuItemStore();
+  const [onSwiperIndex, setOnSwiperIndex] = useState(0);
 
   // 카테고리 선택
   const clickChoiceCategoryHandler = (item: CategoryWithMenuItem) => {
@@ -21,9 +23,27 @@ const MenuItemCategoryPage = () => {
     setMenuItemList(item.menu_item);
   };
 
+  const handleSlideChange = swiper => {
+    // 슬라이드 변경 후에 호출됩니다.
+    setOnSwiperIndex(swiper.activeIndex);
+    // 마지막 슬라이드에 도달했을 때
+    if (swiper.isEnd) {
+      setOnSwiperIndex(-1);
+    }
+  };
+
   return (
     <div className={styles['wrap']}>
-      <Swiper modules={[Virtual]} spaceBetween={10} slidesPerView={'auto'} className={styles['swiper']}>
+      <Swiper
+        modules={[Virtual]}
+        spaceBetween={10}
+        slidesPerView={'auto'}
+        onSlideChange={handleSlideChange}
+        className={clsx(styles.swiper, {
+          [styles.before]: onSwiperIndex !== 0,
+          [styles.after]: onSwiperIndex !== -1,
+        })}
+      >
         {categoryWithMenuItemList.map((item, index) => (
           <SwiperSlide
             key={item.id}
