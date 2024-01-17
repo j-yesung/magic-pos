4c83d49214d8da0c937443128154d34339c09d37
j-yesung <hisung-ah@kakao.com>
2024-01-17 17:00:07 +0900

    🚚 move: 관리자 카테고리 정적 데이터 폴더로 이동 (#103)

--- src/components/layout/admin/AdminLayout.tsx
+++ src/components/layout/admin/AdminLayout.tsx
@@ -1,31 +1,19 @@
+import { adminCategories } from '@/data/admin-categories';
 import useAuthStore from '@/shared/store/auth';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Header from './header/Header';
 import CategoryTitle from './main/CategoryTitle';
-import NavBar from './nav/NavBar';
+import Sidebar from './nav/Sidebar';
 import styles from './styles/AdminLayout.module.css';
 
-interface AdminLayoutProps {
-  children: React.ReactNode;
-}
-
-const adminCategories = [
-  { id: 1, name: '주문 내역 확인', url: '#1', active: false },
-  { id: 2, name: '매출 관리', url: '/admin/sales', active: false },
-  { id: 3, name: '플랫폼 관리', url: '#3', active: false },
-  { id: 4, name: '테이블 관리', url: '/admin/table', active: true },
-  { id: 5, name: '메뉴 관리', url: '/admin/menu-item', active: false },
-  { id: 6, name: '가게 설정', url: '/admin/store', active: false },
-];
-
-const AdminLayout = ({ children }: AdminLayoutProps) => {
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   const { auth } = useAuthStore();
   const router = useRouter();
 
   useEffect(() => {
     if (!auth) router.push('/');
-  }, []);
+  }, [auth, router]);
 
   return (
     <div className={styles.adminWrapper}>
---
+++ src/data/admin-categories.ts
@@ -0,0 +1,9 @@
+export const adminCategories = [
+  { id: 1, name: '주문 내역 확인', url: '/admin/order-check-list', active: false },
+  { id: 2, name: '매출 관리', url: '/admin/sales', active: false },
+  { id: 3, name: '플랫폼 관리', url: '/admin/platform', active: false },
+  { id: 4, name: '테이블 관리', url: '/admin/table', active: true },
+  { id: 5, name: '카테고리 관리', url: '/admin/menu-category', active: false },
+  { id: 6, name: '메뉴 관리', url: '/admin/menu-item', active: false },
+  { id: 7, name: '가게 설정', url: '/admin/store', active: false },
+];
