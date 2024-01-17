344e3cba620b5f4eed7190c003d99115b24d4dc2
j-yesung <hisung-ah@kakao.com>
2024-01-18 00:56:18 +0900

    🚚 move: auth form 데이터 이동

--- src/components/auth/User.tsx
+++ src/components/auth/User.tsx
@@ -1,3 +1,4 @@
+import { FIND_PASSWORD_DATA, LOGIN_DATA, SIGNUP_DATA, UPDATE_PASSWORD_DATA } from '@/data/auth';
 import useAuthStore from '@/shared/store/auth';
 import { useRouter } from 'next/router';
 import AuthForm from './AuthForm';
@@ -10,36 +11,6 @@ const User = () => {
   const router = useRouter();
 
   if (auth) router.push('/');
-
-  const LOGIN_DATA = {
-    url: '/auth/signup',
-    subUrl: '/auth/findPassword',
-    title: '편리함의 시작',
-    subTitle: 'Magic Pos',
-    subName: '비밀번호를 잊으셨나요?',
-    caption: '아직 회원이 아니신가요? 회원가입하러 가기',
-    buttonName: '로그인',
-    buttonSubName: '회원가입',
-  };
-  const SIGNUP_DATA = {
-    url: '/auth/login',
-    title: '편리함의 시작',
-    subTitle: 'Magic Pos',
-    buttonName: '회원가입',
-    subButtonName: '인증하기',
-  };
-  const FIND_PASSWORD_DATA = {
-    subTitle: '비밀번호 찾기',
-    description: '가입하신 이메일을 입력해 주세요.',
-    subDescription: '가입하신 이메일로 비밀번호 재설정 링크를 보내드립니다.',
-    buttonName: '링크 전송',
-  };
-  const UPDATE_PASSWORD_DATA = {
-    title: '편리함의 시작',
-    subTitle: 'Magic Pos',
-    buttonName: '비밀번호 변경',
-    description: '새로운 비밀번호를 입력해 주세요.',
-  };
 
   const AuthData: Record<string, AuthObjectType> = {
     '/auth/login': LOGIN_DATA,
