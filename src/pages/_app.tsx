0918c7b90cddbcd67becef0e1f59b97afab74159
j-yesung <hisung-ah@kakao.com>
2024-01-20 01:10:54 +0900

    ✨ update: next/font/local 전역 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import Modal from '@/components/modal/Modal';
+import Toast from '@/components/toast/Toast';
 import '@/styles/globals.css';
 import '@/styles/reset.css';
 import { NextPageWithLayout } from '@/types/common';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
-import Toast from '@/components/toast/Toast';
+import localFont from 'next/font/local';
+
+const myFont = localFont({ src: './fonts/PretendardVariable.woff2', variable: '--main-font' });
 
 const queryClient = new QueryClient();
 
@@ -18,7 +21,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      {getLayout(<Component {...pageProps} />)}
+      <main className={myFont.variable}>{getLayout(<Component {...pageProps} />)}</main>
       <Modal />
       <Toast />
     </QueryClientProvider>
