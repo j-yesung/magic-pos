9eba99884d16cb1d7d117398228736bfb2d5759d
j-yesung <hisung-ah@kakao.com>
2024-01-20 21:54:19 +0900

    ✨ update: 관리자 페이지 타이틀 동적 생성 (#178)

--- src/components/layout/admin/main/CategoryTitle.tsx
+++ src/components/layout/admin/main/CategoryTitle.tsx
@@ -3,24 +3,28 @@ import clsx from 'clsx';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import styles from '../styles/AdminLayout.module.css';
+import CategoryTitleIcon from './CategoryTitleIcon';
 
 const CategoryTitle = (adminInfo: AdminCategories) => {
   const path = useRouter().pathname;
   const info = adminInfo.adminCategories.find(item => item.url === path);
   const auth = useAuthStore(state => state.auth);
-  const [isManagementPathName, setIsManagementPathName] = useState(false)
+  const [isManagementPathName, setIsManagementPathName] = useState(false);
 
   useEffect(() => {
     if (path === '/admin/management') {
-      setIsManagementPathName(true)
+      setIsManagementPathName(true);
     } else {
-      setIsManagementPathName(false)
+      setIsManagementPathName(false);
     }
   }, [auth, path, isManagementPathName]);
 
   return (
     <section className={clsx(isManagementPathName && styles.categoryTitleWrapper)}>
-      <h1 className={styles.categoryTitle}>{info && info.name}</h1>
+      <div className={styles.iconWrapper}>
+        <CategoryTitleIcon />
+        <h1 className={styles.categoryTitle}>{info && info.name}</h1>
+      </div>
     </section>
   );
 };
---
+++ src/components/layout/admin/main/CategoryTitleIcon.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import { IoBrowsersOutline, IoCalendarClearOutline, IoDocumentOutline, IoGrid } from 'react-icons/io5';
+
+const ICONS: Record<string, JSX.Element> = {
+  '/admin/order-check-list': <IoDocumentOutline size={50} />,
+  '/admin/sales': <IoCalendarClearOutline size={50} />,
+  '/admin/platform': <IoBrowsersOutline size={50} />,
+  '/admin/table': <IoGrid size={50} />,
+};
+
+const CategoryTitleIcon = () => {
+  const path = useRouter().pathname;
+  const icon = ICONS[path];
+
+  return <>{icon}</>;
+};
+
+export default CategoryTitleIcon;
