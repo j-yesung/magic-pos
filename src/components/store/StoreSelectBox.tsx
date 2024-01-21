60c1be5232fe5b380b82c381fc9e7a8b08102583
j-yesung <hisung-ah@kakao.com>
2024-01-21 19:57:06 +0900

    ✨ update: 셀렉트 박스 컴포넌트 생성 및 모듈화 (#186)

---
+++ src/components/store/StoreSelectBox.tsx
@@ -0,0 +1,58 @@
+import Select, { StylesConfig } from 'react-select';
+import { createTimeOptions, timeOption } from '../utils/time-format';
+import { TimeState } from './StoreTimeSet';
+
+interface OptionType {
+  value: string;
+  label: string;
+}
+
+interface StoreSelectBoxProps {
+  times: TimeState;
+  item: { id: number; name: string; label: string; defaultValue: string };
+  setTimes: React.Dispatch<React.SetStateAction<TimeState>>;
+}
+
+const customStyles: StylesConfig<OptionType, false> = {
+  control: provided => ({
+    ...provided,
+    width: '16rem',
+    height: '5rem',
+    fontWeight: '500',
+    fontSize: '1.5rem',
+    textAlign: 'center',
+    color: '#000000',
+    background: '#fff',
+    borderRadius: '0.5rem',
+    boxShadow: '0 0 1rem 0 rgba(0, 0, 0, 0.10)',
+  }),
+  menu: provided => ({
+    ...provided,
+    width: '16rem',
+    fontWeight: '500',
+    fontSize: '1.5rem',
+    color: '#000000',
+    textAlign: 'center',
+    borderRadius: '0.5rem',
+  }),
+};
+
+const StoreSelectBox = (props: StoreSelectBoxProps) => {
+  const { times, item, setTimes } = props;
+  const updateTimeState = (name: string, value: string) => setTimes(prevTimes => ({ ...prevTimes, [name]: value }));
+  const timeOptions = createTimeOptions(timeOption);
+
+  return (
+    <Select
+      styles={customStyles}
+      name={item.name}
+      id={item.name}
+      onChange={option => updateTimeState(item.name, option?.value ?? '')}
+      value={timeOptions.find(option => option.value === times[item.name])}
+      options={timeOptions.filter(option => (item.name === 'endTime' ? option.value > times.startTime : true))}
+      placeholder={item.defaultValue}
+    />
+  );
+};
+
+export default StoreSelectBox;
--- src/components/store/StoreTimeSet.tsx
+++ src/components/store/StoreTimeSet.tsx
@@ -1,18 +1,22 @@
 import { useGetQuery } from '@/hooks/store/useGetQuery';
 import { useStoreQuery } from '@/hooks/store/useStoreQuery';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useCallback, useEffect, useState } from 'react';
 import Button from '../common/Button';
+import { convertTimeFormat, timeSet } from '../utils/time-format';
+import StoreSelectBox from './StoreSelectBox';
 import styles from './styles/StroeContents.module.css';
 
-const TIME_SIZE = 48;
-
-type TimeState = Record<string, string>;
+export type TimeState = Record<string, string>;
 
 const StoreTimeSet = ({ userId }: { userId: string }) => {
-  const { updateStoreTimeSet } = useStoreQuery();
   const { data } = useGetQuery({ userId });
+  const { updateStoreTimeSet } = useStoreQuery();
   const [times, setTimes] = useState<TimeState>({ startTime: '', endTime: '' });
+  const openTime = convertTimeFormat(times.startTime).convertedTime;
+  const closeTime = convertTimeFormat(times.endTime).convertedTime;
+  const isTimeSet = times.startTime === '' || times.endTime === '';
 
+  // DB 영업시간 불러오기
   useEffect(() => {
     if (data) {
       setTimes({
@@ -22,71 +26,36 @@ const StoreTimeSet = ({ userId }: { userId: string }) => {
     }
   }, [data]);
 
-  // 24시간 30분 단위로 시간을 표시
-  const time = Array.from(
-    { length: TIME_SIZE },
-    (_, i) => `${String(Math.floor(i / 2)).padStart(2, '0')}:${String((i % 2) * 30).padStart(2, '0')}:00`,
-  );
-
-  const timeSet = [
-    { id: 1, name: 'startTime', label: '부터' },
-    { id: 2, name: 'endTime', label: '까지' },
-  ];
-
-  const convertTimeFormat = (time: string) => {
-    const [hour, minute] = time.split(':');
-    const hourNumber = Number(hour);
-    const prefix = hourNumber >= 12 ? '오후' : '오전';
-    const convertedHour = hourNumber > 12 ? hourNumber - 12 : hourNumber === 0 ? 12 : hourNumber;
-
-    return {
-      originalTime: time,
-      convertedTime: `${prefix} ${convertedHour}:${minute}`,
-    };
-  };
-
-  const clickUpdateStoreHandler = () => {
+  // DB 영업시간 변경
+  const clickUpdateStoreHandler = useCallback(() => {
     updateStoreTimeSet({ userId, ...times });
-  };
-
-  const changeTimeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setTimes({
-      ...times,
-      [e.target.name]: e.target.value,
-    });
-  };
+  }, [times, updateStoreTimeSet, userId]);
 
   return (
     <>
       <div className={styles.timesetContainer}>
-        <label htmlFor="tiem">영업시간</label>
+        <label htmlFor="time">영업시간</label>
         <div className={styles.timeSelectBox}>
-          {timeSet.map(item => (
-            <Fragment key={item.id}>
-              {data && data?.length > 0 && (
-                <>
-                  <select name={item.name} id={item.name} onChange={changeTimeHandler} value={times[item.name]}>
-                    {time
-                      .filter(t => (item.name === 'endTime' ? t > times.startTime : true))
-                      .map((t, index) => (
-                        <option key={index} value={t}>
-                          {convertTimeFormat(t).convertedTime}
-                        </option>
-                      ))}
-                  </select>
+          {data && data?.length > 0 && (
+            <>
+              {timeSet.map(item => (
+                <Fragment key={item.id}>
+                  <StoreSelectBox item={item} times={times} setTimes={setTimes} />
                   <span>{item.label}</span>
-                </>
-              )}
-            </Fragment>
-          ))}
+                </Fragment>
+              ))}
+            </>
+          )}
         </div>
         <Button type="button" onClick={clickUpdateStoreHandler} className={styles.buttonBlankArea}>
           수정
         </Button>
       </div>
       <p className={styles.timeCaption}>
-        현재 {convertTimeFormat(times.startTime).convertedTime} 부터 {convertTimeFormat(times.endTime).convertedTime}
-        까지로 영업시간이 설정되었습니다.
+        {isTimeSet
+          ? '영업시간을 선택해 주세요.'
+          : `현재 ${openTime} 부터 ${closeTime}
+        까지로 영업시간이 설정되었습니다.`}
       </p>
     </>
   );
