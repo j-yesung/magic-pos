53a34b955b2fbc02209a26ef7dd0a2b4f36cab35
sanghyeon <uphyeon@kakao.com>
2024-01-22 21:03:21 +0900

    :recycle: refactor: toast 관련 상태, 액션 분리 (#105)

--- src/shared/store/toast.ts
+++ src/shared/store/toast.ts
@@ -7,26 +7,30 @@ import { ToastAnimationType, ToastTypeOption } from '@/types/common';
 
 interface ToastState {
   toastList: ToastTypeOption[];
-  addToastList: (elem: ToastTypeOption) => void;
-  subtractToastList: (id: string) => void;
-  // 해당 Toast에게 어떤 종류의 애니메이션을 실행할지 지정합니다.
-  setAnimation: (id: string, type: ToastAnimationType) => void;
 }
 
 const useToastStore = create<ToastState>()(set => ({
   toastList: [],
-  addToastList: (toast: ToastTypeOption) => set(state => ({ toastList: [...state.toastList, toast] })),
-  subtractToastList: (id: string) =>
-    set(state => ({
-      toastList: state.toastList.filter(toast => toast.id !== id),
-    })),
-  setAnimation: (id: string, type: ToastAnimationType) =>
-    set(state => ({
-      toastList: state.toastList.map(toast => {
-        if (toast.id === id) return { ...toast, animation: type };
-        return toast;
-      }),
-    })),
 }));
+
+export const addToastList = (toast: ToastTypeOption) => {
+  useToastStore.setState(state => ({ toastList: [...state.toastList, toast] }));
+};
+
+export const subtractToastList = (id: string) => {
+  useToastStore.setState(state => ({
+    toastList: state.toastList.filter(toast => toast.id !== id),
+  }));
+};
+
+// 해당 Toast에게 어떤 종류의 애니메이션을 실행할지 지정합니다.
+export const setAnimation = (id: string, type: ToastAnimationType) => {
+  useToastStore.setState(state => ({
+    toastList: state.toastList.map(toast => {
+      if (toast.id === id) return { ...toast, animation: type };
+      return toast;
+    }),
+  }));
+};
 
 export default useToastStore;
