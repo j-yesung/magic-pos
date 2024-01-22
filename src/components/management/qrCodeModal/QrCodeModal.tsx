23183f6eb76f2ce0740bd517725f742bedd15070
j-yesung <hisung-ah@kakao.com>
2024-01-22 10:02:11 +0900

    🩹 fix: QR 코드 모달 버튼 별 영역 분기처리 (#205)

--- src/components/management/qrCodeModal/QrCodeModal.tsx
+++ src/components/management/qrCodeModal/QrCodeModal.tsx
@@ -1,15 +1,25 @@
 import useQRCodeDownLoad from '@/hooks/management/useQRCodeDownLoad';
 import { useModal } from '@/hooks/modal/useModal';
 import useManagementStore from '@/shared/store/management';
+import { useState } from 'react';
 import PackagingQrCodeContainer from './packagingQrCodeContainer/PackagingQrCodeContainer';
 import ShopQrCodeContainer from './shopQrCodeContainer/ShopQrCodeContainer';
 import styles from './styles/QrCodeModal.module.css';
 import CloseButton from '/public/icons/x.svg';
 
+type QrCodeModalProps = 'shop' | 'packaging' | null;
+
 const QrCodeModal = ({ modalId }: { modalId?: string }) => {
   const { qrData } = useManagementStore();
-  const qrDownLoad = useQRCodeDownLoad();
+  const qrDownLoad: ReturnType<typeof useQRCodeDownLoad> = useQRCodeDownLoad();
   const { MagicModal } = useModal();
+  const [selectedComponent, setSelectedComponent] = useState<QrCodeModalProps>(null);
+
+  const clickComponentHandler = (component: 'shop' | 'packaging') => {
+    if (component === selectedComponent) return;
+    setSelectedComponent(prevComponent => (prevComponent === component ? null : component));
+  };
+
   const clickQrDownLoadHandler = () => {
     qrData.forEach(item => {
       qrDownLoad(item);
@@ -20,20 +30,38 @@ const QrCodeModal = ({ modalId }: { modalId?: string }) => {
   };
 
   return (
-    <div className={styles.qrCodeModalBox}>
-      <div className={styles.qrTitleContainer}>
-        <div className={styles.qrCodeTitle}>QR코드 출력하기</div>
-        <CloseButton width={20} height={20} />
+    <>
+      <div className={styles.qrCodeModalBox}>
+        <div className={styles.qrTitleContainer}>
+          <div className={styles.qrCodeTitle}>QR코드 출력하기</div>
+          <CloseButton width={20} height={20} />
+        </div>
+        <div className={styles.qrCodeBox}>
+          <div
+            className={selectedComponent === 'shop' ? `${styles.qrCodeMoveButton} ${styles.active}` : styles.notActive}
+            onClick={() => clickComponentHandler('shop')}
+          >
+            매장용 QR코드
+          </div>
+          <div
+            className={
+              selectedComponent === 'packaging' ? `${styles.qrCodeMoveButton} ${styles.active}` : styles.notActive
+            }
+            onClick={() => clickComponentHandler('packaging')}
+          >
+            포장용 QR코드
+          </div>
+        </div>
+        <div className={styles.qrCodeShowBox}>
+          {selectedComponent === 'shop' && <ShopQrCodeContainer />}
+          {selectedComponent === 'packaging' && <PackagingQrCodeContainer />}
+        </div>
+        <div className={styles.qrCodeButtonBox}>
+          <button onClick={clickQrDownLoadHandler}>전체 다운로드</button>
+          <button onClick={clickModalCloseHandler}>닫기</button>
+        </div>
       </div>
-      <div className={styles.qrCodeBox}>
-        <ShopQrCodeContainer />
-        <PackagingQrCodeContainer />
-      </div>
-      <div className={styles.qrCodeButtonBox}>
-        <button onClick={clickQrDownLoadHandler}>QR코드 다운로드</button>
-        <button onClick={clickModalCloseHandler}>닫기</button>
-      </div>
-    </div>
+    </>
   );
 };
 
