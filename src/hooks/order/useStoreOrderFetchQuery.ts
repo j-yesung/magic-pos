280274ff590d9a1c1fefee93640eb8c5fcdbbb97
sanghyeon <uphyeon@kakao.com>
2024-01-23 17:08:31 +0900

    :recycle: refactor: refetch false 옵션 추가 (#232)

--- src/hooks/order/useStoreOrderFetchQuery.ts
+++ src/hooks/order/useStoreOrderFetchQuery.ts
@@ -8,6 +8,7 @@ export const useStoreOrderFetchQuery = (orderId: string[], storeId: string) => {
   const { data: storeOrderData } = useQuery({
     queryKey: [QUERY_KEY, orderId],
     enabled: orderId.length > 0,
+    refetchOnWindowFocus: false,
     queryFn: () => {
       return fetchStoreOrderByOrderIdWithStoreName(orderId, storeId);
     },
