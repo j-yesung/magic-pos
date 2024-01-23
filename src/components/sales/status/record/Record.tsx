68109584e88bfc86a0eb6eaf00f023b4f6e94f47
lunaxislu <cmk0905@naver.com>
2024-01-24 03:56:21 +0900

    🚚  recordType으로 기능 이동

--- src/components/sales/status/record/Record.tsx
+++ src/components/sales/status/record/Record.tsx
@@ -1,8 +1,9 @@
 import { convertNumberToWon } from '@/shared/helper';
 import useCalendarStore from '@/shared/store/sales/calendar';
 import useRecordStore from '@/shared/store/sales/record';
-import moment, { Moment } from 'moment';
+import { Moment } from 'moment';
 import { useEffect, useRef } from 'react';
+import { formattedRecordType } from '../../calendarUtility/recordType';
 import styles from './styles/Record.module.css';
 
 const Record = () => {
@@ -10,17 +11,7 @@ const Record = () => {
   const selectedDate = useCalendarStore(state => state.selectedDate);
   const dateRef = useRef<Moment | null>(null);
 
-  const formattedType = (dateType: 'day' | 'week' | 'month', formatType: 'M월 D일' | 'YYYY년 M월 D일') => {
-    const SALES_TYPE = {
-      day: moment().isSame(dateRef.current, 'date') ? '오늘' : dateRef.current?.format(formatType),
-      week: '이번 주',
-      month: '이번 달',
-    };
-
-    return SALES_TYPE[dateType];
-  };
   useEffect(() => {
-    console.log('dffffffffffffffff');
     if (!dateRef.current) {
       dateRef.current = selectedDate;
     }
@@ -30,14 +21,14 @@ const Record = () => {
       }
     };
   }, [selectedDate]);
-  console.log(record);
+
   return (
     <div className={styles.recordContainer}>
       <div className={styles.recordSalesWrapper}>
-        {formattedType(record.dateType, 'M월 D일')}의 매출은
+        {formattedRecordType(dateRef.current, record.dateType, 'M월 D일')}의 매출은
         <span className={styles.recordSales}>{convertNumberToWon(record.currentSales)}</span>입니다.
       </div>
-      <div className={styles.recordDate}>{formattedType(record.dateType, 'YYYY년 M월 D일')}</div>
+      <div className={styles.recordDate}>{formattedRecordType(dateRef.current, record.dateType, 'YYYY년 M월 D일')}</div>
       <div>{/* 위 태그를 가운데로 맞추려고 일부러 넣은 div */}</div>
     </div>
   );
