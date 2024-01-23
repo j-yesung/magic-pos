9deda5121e875d18342d4e479156d16400f156c3
j-yesung <hisung-ah@kakao.com>
2024-01-23 15:20:40 +0900

    ✨ update: 로그인 페이지 - 이메일 저장 로직 구현 (#231)

--- src/components/auth/element/SignCaption.tsx
+++ src/components/auth/element/SignCaption.tsx
@@ -1,27 +1,46 @@
 import Checkbox from '@/components/common/Checkbox';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Auth.module.css';
 
 interface SignCaptionProps {
   subUrl: string;
   subName: string;
+  value: Record<string, string>;
+  isCheckbox: boolean;
+  setIsCheckbox: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const SignCaption = (props: SignCaptionProps) => {
-  const { subUrl, subName } = props;
+  const { subUrl, subName, value, isCheckbox, setIsCheckbox } = props;
+  const { email } = value;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const clickStorageSaveHandler = () => {
+    setIsCheckbox(prev => !prev);
+    isCheckbox ? localStorage.removeItem('email') : localStorage.setItem('email', email);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
 
   return (
-    <div className={styles.captionBox}>
-      <div className={styles.checkbox}>
-        <Checkbox />
-        <p>이메일 저장</p>
-      </div>
-      <div className={styles.findPasswordBox}>
-        <Link className={styles.caption} href={subUrl}>
-          {subName}
-        </Link>
-      </div>
-    </div>
+    <>
+      {isLoading && (
+        <div className={styles.captionBox}>
+          <div className={styles.checkbox}>
+            <Checkbox onClick={clickStorageSaveHandler} defaultChecked={isCheckbox} />
+            <p>이메일 저장</p>
+          </div>
+          <div className={styles.findPasswordBox}>
+            <Link className={styles.caption} href={subUrl}>
+              {subName}
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
