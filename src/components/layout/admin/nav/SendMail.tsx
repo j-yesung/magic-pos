16586420305c4dbe0c8d776c109168dedb8893c8
j-yesung <hisung-ah@kakao.com>
2024-01-24 20:13:15 +0900

    🩹 fix: useInput 적용 (#271)

--- src/components/layout/admin/nav/SendMail.tsx
+++ src/components/layout/admin/nav/SendMail.tsx
@@ -1,22 +1,24 @@
+import Button from '@/components/common/Button';
+import { useInput } from '@/hooks/service/auth/useInput';
 import { useSendMail } from '@/hooks/service/useSendEmail';
+import useAuthState from '@/shared/store/session';
 import { useRef } from 'react';
 
 const SendMail = () => {
   const form = useRef<HTMLFormElement>(null);
   const { sendEmail } = useSendMail({ ref: form });
+  const storeName = useAuthState(state => state.storeName);
+  const { value, changeHandler } = useInput({ user_email: '' });
 
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <label>상호명</label>
-      <input type="text" name="to_name" value="삼성전자" readOnly />
-
-      <label>답변 받으실 이메일을 작성해 주세요.</label>
-      <input type="email" name="user_email" />
-
+      <label htmlFor="to_name">상호명</label>
+      <input type="text" id="to_name" name="to_name" value={storeName ?? ''} disabled />
+      <label htmlFor="user_email">답변 받으실 이메일을 작성해 주세요.</label>
+      <input type="email" id="user_email" name="user_email" value={value.user_email} onChange={changeHandler} />
       <label>문의하실 내용을 작성해 주세요.</label>
-      <textarea name="message" />
-
-      <input type="submit" value="Send" />
+      <textarea id="message" name="message" />
+      <Button type="submit">보내기</Button>
     </form>
   );
 };
