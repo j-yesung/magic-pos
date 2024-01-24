24bf6c132cc95bb6afa88a25ec6bf37a41a5f4e4
j-yesung <hisung-ah@kakao.com>
2024-01-24 19:21:46 +0900

    🩹 fix: 메일 커스텀 훅 변수 분리 (#271)

--- src/hooks/service/useSendEmail.ts
+++ src/hooks/service/useSendEmail.ts
@@ -6,6 +6,10 @@ interface SendMailProps {
   ref: MutableRefObject<HTMLFormElement | null>;
 }
 
+const SERVICE_ID = `${process.env.NEXT_PUBLIC_SERVICE_ID}`;
+const TEMPLATE_ID = `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`;
+const PUBLIC_KEY = `${process.env.NEXT_PUBLIC_PUBLIC_KEY}`;
+
 export const useSendMail = ({ ref }: SendMailProps) => {
   const { toast } = useToast();
 
@@ -13,35 +17,28 @@ export const useSendMail = ({ ref }: SendMailProps) => {
     e.preventDefault();
     if (!ref.current) return;
 
-    emailjs
-      .sendForm(
-        `${process.env.NEXT_PUBLIC_SERVICE_ID}`,
-        `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`,
-        ref.current,
-        `${process.env.NEXT_PUBLIC_PUBLIC_KEY}`,
-      )
-      .then(
-        result => {
-          if (result.text === 'OK') {
-            toast('메일이 전송되었습니다.', {
-              type: 'success',
-              position: 'top-right',
-              showCloseButton: false,
-              autoClose: 2000,
-            });
-          }
-        },
-        error => {
-          if (error) {
-            toast('메일 전송에 실패했습니다.', {
-              type: 'danger',
-              position: 'top-right',
-              showCloseButton: false,
-              autoClose: 2000,
-            });
-          }
-        },
-      );
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, ref.current, PUBLIC_KEY).then(
+      result => {
+        if (result.text === 'OK') {
+          toast('메일이 전송되었습니다.', {
+            type: 'success',
+            position: 'top-right',
+            showCloseButton: false,
+            autoClose: 2000,
+          });
+        }
+      },
+      error => {
+        if (error) {
+          toast('메일 전송에 실패했습니다.', {
+            type: 'danger',
+            position: 'top-right',
+            showCloseButton: false,
+            autoClose: 2000,
+          });
+        }
+      },
+    );
   };
 
   return { sendEmail };
