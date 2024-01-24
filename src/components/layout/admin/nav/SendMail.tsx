f9b07d42b44745c2fca464c43801d15fd795431c
j-yesung <hisung-ah@kakao.com>
2024-01-24 20:25:58 +0900

    💄 style: 뼈대 생성 (#271)

--- src/components/layout/admin/nav/SendMail.tsx
+++ src/components/layout/admin/nav/SendMail.tsx
@@ -3,6 +3,7 @@ import { useInput } from '@/hooks/service/auth/useInput';
 import { useSendMail } from '@/hooks/service/useSendEmail';
 import useAuthState from '@/shared/store/session';
 import { useRef } from 'react';
+import styles from '../styles/SendMail.module.css';
 
 const SendMail = () => {
   const form = useRef<HTMLFormElement>(null);
@@ -11,15 +12,24 @@ const SendMail = () => {
   const { value, changeHandler } = useInput({ user_email: '' });
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label htmlFor="to_name">상호명</label>
-      <input type="text" id="to_name" name="to_name" value={storeName ?? ''} disabled />
-      <label htmlFor="user_email">답변 받으실 이메일을 작성해 주세요.</label>
-      <input type="email" id="user_email" name="user_email" value={value.user_email} onChange={changeHandler} />
-      <label>문의하실 내용을 작성해 주세요.</label>
-      <textarea id="message" name="message" />
-      <Button type="submit">보내기</Button>
-    </form>
+    <div className={styles.wrapper}>
+      <h1>문의사항</h1>
+      <form className={styles.formBox} ref={form} onSubmit={sendEmail}>
+        <div className={styles.inputBox}>
+          <label htmlFor="to_name">상호명</label>
+          <input type="text" id="to_name" name="to_name" value={storeName ?? ''} disabled />
+        </div>
+        <div className={styles.inputBox}>
+          <label htmlFor="user_email">답변 받으실 이메일을 작성해 주세요.</label>
+          <input type="email" id="user_email" name="user_email" value={value.user_email} onChange={changeHandler} />
+        </div>
+        <div className={styles.inputBox}>
+          <label>문의하실 내용을 작성해 주세요.</label>
+          <textarea id="message" name="message" />
+        </div>
+        <Button type="submit">보내기</Button>
+      </form>
+    </div>
   );
 };
 
