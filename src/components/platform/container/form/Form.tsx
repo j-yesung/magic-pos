920b91d506b411617911cd3864efe68dcbc27bbd
lunaxislu <cmk0905@naver.com>
2024-01-25 11:45:01 +0900

    🚚  플랫폼 등록 기능 이동

--- src/components/platform/container/form/Form.tsx
+++ src/components/platform/container/form/Form.tsx
@@ -1,54 +1,12 @@
 import Button from '@/components/common/Button';
-import { downloadPlatFormImageUrl, insertPlatFormRow, uploadPlatFormImage } from '@/server/api/supabase/platform';
-import {
-  resetAddPlatForm,
-  setAddDataToFetchPlatForm,
-  setIsRegist,
-  default as usePlatformStore,
-} from '@/shared/store/platform';
-import { AddPlatFormType } from '@/types/platform';
+import usePlatForm from '@/hooks/platform/usePlatForm';
 import clsx from 'clsx';
-import moment from 'moment';
-import { FormEvent } from 'react';
 import ImgForm from './img/ImgForm';
 import Input from './input/Input';
 import styles from './styles/form.module.css';
 import CloseButton from '/public/icons/close.svg';
 const Form = () => {
-  const addPlatform = usePlatformStore(state => state.addPlatForm);
-
-  const submitAddCard = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // image 경로로 사용하기 위해 createdAt를 넣었습니다.
-    let updateData: AddPlatFormType = { ...addPlatform, createdAt: moment().toISOString() };
-    /**useModal 사용하기 */
-    if (!addPlatform.name.trim() || !addPlatform.link_url.trim()) return alert('써라');
-
-    if (updateData.file) {
-      await uploadPlatFormImage(updateData);
-      const { publicUrl: image_url } = downloadPlatFormImageUrl(updateData);
-      updateData = {
-        ...addPlatform,
-        image_url,
-      };
-      const { data } = await insertPlatFormRow(updateData);
-      setAddDataToFetchPlatForm(data!);
-    } else {
-      console.log(updateData);
-      const { file, createdAt, ...insertData } = updateData;
-      console.log(insertData);
-      const { data } = await insertPlatFormRow(updateData);
-      setAddDataToFetchPlatForm(data!);
-    }
-    resetAddPlatForm();
-    setIsRegist(false);
-  };
-
-  const closeAddFormModal = () => {
-    setIsRegist(false);
-    resetAddPlatForm();
-  };
+  const { submitAddCard, closeAddFormModal } = usePlatForm();
 
   return (
     <form onSubmit={submitAddCard} className={styles.formContainer}>
---
+++ src/hooks/platform/usePlatForm.ts
@@ -0,0 +1,47 @@
+import { downloadPlatFormImageUrl, insertPlatFormRow, uploadPlatFormImage } from '@/server/api/supabase/platform';
+import usePlatFormStore, { resetAddPlatForm, setAddDataToFetchPlatForm, setIsRegist } from '@/shared/store/platform';
+import { AddPlatFormType } from '@/types/platform';
+import moment from 'moment';
+import { FormEvent } from 'react';
+
+const usePlatForm = () => {
+  const addPlatform = usePlatFormStore(state => state.addPlatForm);
+
+  const submitAddCard = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // image 경로로 사용하기 위해 createdAt를 넣었습니다.
+    let updateData: AddPlatFormType = { ...addPlatform, createdAt: moment().toISOString() };
+    /**useModal 사용하기 */
+    if (!addPlatform.name.trim() || !addPlatform.link_url.trim()) return alert('써라');
+
+    if (updateData.file) {
+      await uploadPlatFormImage(updateData);
+      const { publicUrl: image_url } = downloadPlatFormImageUrl(updateData);
+      updateData = {
+        ...addPlatform,
+        image_url,
+      };
+      const { data } = await insertPlatFormRow(updateData);
+      setAddDataToFetchPlatForm(data!);
+    } else {
+      const { file, createdAt, ...insertData } = updateData;
+      const { data } = await insertPlatFormRow(updateData);
+      setAddDataToFetchPlatForm(data!);
+    }
+    resetAddPlatForm();
+    setIsRegist(false);
+  };
+
+  const closeAddFormModal = () => {
+    setIsRegist(false);
+    resetAddPlatForm();
+  };
+
+  return {
+    submitAddCard,
+    closeAddFormModal,
+  };
+};
+
+export default usePlatForm;
