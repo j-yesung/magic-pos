050f53429d0463dff13f0d2c2f5d71fae1514628
LeeDongTak <sunny5796@naver.com>
2024-01-25 21:35:02 +0900

    💄 add: LoadingSpinner 컴포넌트 생성 (#301)

--- src/components/management/managementSideBar/OrderItem.tsx
+++ src/components/management/managementSideBar/OrderItem.tsx
@@ -1,3 +1,4 @@
+import LoadingSpinner from '@/components/common/LoadingSpinner';
 import useSetManagement from '@/hooks/management/useSetManagement';
 import { useModal } from '@/hooks/modal/useModal';
 import { groupByKey } from '@/shared/helper';
@@ -10,7 +11,7 @@ const OrderItem = ({ orderData }: { orderData: OrderDataWithStoreName }) => {
   const { id, order_number, order_time, menu_list, total_price, is_togo } = orderData;
   const menuList: MenuItemWithOption[] = JSON.parse(JSON.stringify(menu_list));
   const { MagicModal } = useModal();
-  const { mutate } = useSetManagement();
+  const { mutate, isPending } = useSetManagement();
 
   const group = groupByKey<MenuItemWithOption>(menuList, 'unique');
 
@@ -37,7 +38,6 @@ const OrderItem = ({ orderData }: { orderData: OrderDataWithStoreName }) => {
         {[...group]?.map(([key, item]) => {
           return (
             <li key={key} className={styles['menu-list-item']}>
-              
               <div className={styles['menu-name']}>
                 <span>{item[0].name}</span>
                 <span>{item.length}</span>
@@ -64,8 +64,14 @@ const OrderItem = ({ orderData }: { orderData: OrderDataWithStoreName }) => {
         {/*  <span>{total_price} 원</span>*/}
         {/*</div>*/}
         <button onClick={clickOrderConfirmHandler}>
-          <IoCheckmark className={styles['check-icon']} />
-          주문완료하기
+          {isPending ? (
+            <LoadingSpinner boxSize={2} ballSize={0.4} interval={1.5} />
+          ) : (
+            <>
+              <IoCheckmark className={styles['check-icon']} />
+              <span>주문완료하기</span>
+            </>
+          )}
         </button>
       </div>
     </li>
