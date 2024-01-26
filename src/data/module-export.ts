57c111e8a50a5cc811d35177bb79e4bde51f3788
j-yesung <hisung-ah@kakao.com>
2024-01-27 00:15:23 +0900

    🩹 fix: 키오스크 화면 소개 문구 수정 (#294)

---
+++ src/data/module-export.ts
@@ -0,0 +1,5 @@
+import MenuShot from '/public/images/iphone-shot/menu.svg';
+import SelectShot from '/public/images/iphone-shot/select.svg';
+import StartShot from '/public/images/iphone-shot/start.svg';
+
+export { MenuShot, SelectShot, StartShot };
