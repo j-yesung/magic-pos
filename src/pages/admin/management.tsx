a23c44a26fa4ce808784420afec3dc3c33072483
LeeDongTak <sunny5796@naver.com>
2024-01-27 21:17:39 +0900

    🎉 update: management,order-check-list-table 페이지에 next/head 테그 삽입 (#343)

--- src/pages/admin/management.tsx
+++ src/pages/admin/management.tsx
@@ -1,7 +1,6 @@
 import AdminLayout from '@/components/layout/admin/AdminLayout';
 import Management from '@/components/management/Management';
 import Head from 'next/head';
-import { ReactNode } from 'react';
 
 const ManagementPage = () => {
   return (
@@ -17,6 +16,9 @@ const ManagementPage = () => {
         <title>magic pos 운영모드</title>
         <meta name="description" content="NextJS Events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="파워레인조" />
+        <meta name="keywords" content="magic pos,매직 포스, 키오스크, 운영모드, 주문관리, 운영관리" />
+        <meta name="description" content="magic pos의 운영모드 페이지 입니다. " />
       </Head>
       <Management />
     </>
--- src/pages/admin/table/index.tsx
+++ src/pages/admin/table/index.tsx
@@ -5,14 +5,17 @@ import { ReactNode } from 'react';
 
 const TablePage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>magic pos 테이블 관리</title>
         <meta name="description" content="NextJS Events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="파워레인조" />
+        <meta name="keywords" content="magic pos,매직 포스, 키오스크, 테이블 관리" />
+        <meta name="description" content="magic pos의 테이블을 관리하는 페이지 입니다. " />
       </Head>
       <Table />
-    </div>
+    </>
   );
 };
 
