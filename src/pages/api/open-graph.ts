3d64200e90a2dbab5a498fbf091a09269533f229
lunaxislu <cmk0905@naver.com>
2024-01-29 00:15:04 +0900

    🔥  주석 불필요한 것 삭제

--- src/pages/api/open-graph.ts
+++ src/pages/api/open-graph.ts
@@ -11,7 +11,6 @@ export default async function handler(req: ExtendsNextApiRequest, res: NextApiRe
   const options = { url };
   try {
     const { result } = await ogs(options);
-    console.log('asidjfiasdj', result);
     if (result) {
       res.status(200).send(result.ogImage![0].url);
     }
@@ -19,15 +18,3 @@ export default async function handler(req: ExtendsNextApiRequest, res: NextApiRe
     res.status(500).send('링크의 정보를 가져오는데 실패했습니다.');
   }
 }
-
-// const getOpenMeta = async (url: string) => {
-//   try {
-//     ogs;
-//     const { data } = await axios.get(url);
-//     const $ = cheerio.load(data);
-//     // 웹 페이지의 meta Image입니다.
-//     return $('meta[property="og:image"]').attr('content');
-//   } catch (error) {
-//     throw error;
-//   }
-// };
