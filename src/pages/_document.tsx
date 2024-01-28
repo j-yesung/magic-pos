c8562d419e2e9241bda1ecedaea010e4b0fab546
j-yesung <hisung-ah@kakao.com>
2024-01-28 19:05:32 +0900

    ✨ update: 스플래시 이미지 교체

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,52 +10,52 @@ export default function Document() {
       <link rel="apple-touch-icon" href="/icons/favicon/apple-icon.png"></link>
       <meta name="msapplication-TileColor" content="#FF98BA"></meta>
       <link
-        href="/icons/splashscreens/iphone5_splash.png"
+        href="splashscreens/iphone5_splash.png"
         media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/iphone6_splash.png"
+        href="splashscreens/iphone6_splash.png"
         media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/iphoneplus_splash.png"
+        href="splashscreens/iphoneplus_splash.png"
         media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/iphonex_splash.png"
+        href="splashscreens/iphonex_splash.png"
         media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/iphonexr_splash.png"
+        href="splashscreens/iphonexr_splash.png"
         media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/iphonexsmax_splash.png"
+        href="splashscreens/iphonexsmax_splash.png"
         media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/ipad_splash.png"
+        href="splashscreens/ipad_splash.png"
         media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/ipadpro1_splash.png"
+        href="splashscreens/ipadpro1_splash.png"
         media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/ipadpro3_splash.png"
+        href="splashscreens/ipadpro3_splash.png"
         media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <link
-        href="/icons/splashscreens/ipadpro2_splash.png"
+        href="splashscreens/ipadpro2_splash.png"
         media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
