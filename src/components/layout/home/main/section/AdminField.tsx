a2271bdd9232cc188c9da171e2a4fbef3ae00b12
j-yesung <hisung-ah@kakao.com>
2024-01-30 02:36:09 +0900

    🩹 fix: 관리자 페이지 스와이퍼 이미지, 텍스트 분리 (#294)

--- src/components/layout/home/main/section/AdminField.tsx
+++ src/components/layout/home/main/section/AdminField.tsx
@@ -1,20 +1,23 @@
 import { ADMIN_INFO } from '@/data/scroll-props';
 import Image from 'next/image';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
+import { Swiper as SwiperType } from 'swiper/types';
 import styles from '../../styles/Section.module.css';
 import PrevArrow from '/public/icons/arrow-left.svg';
 import NextArrow from '/public/icons/arrow-right.svg';
 
 const AdminField = () => {
   const swiperRef = useRef<SwiperRef>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const goNext = () => swiperRef?.current?.swiper.slideNext();
   const goPrev = () => swiperRef?.current?.swiper.slidePrev();
+  const changeSlideHandler = (swiper: SwiperType) => setActiveIndex(swiper.realIndex);
 
   return (
     <section className={styles.adminBox}>
@@ -30,15 +33,31 @@ const AdminField = () => {
           }}
           loop={true}
           ref={swiperRef}
+          onSlideChange={changeSlideHandler}
         >
           {ADMIN_INFO.map(info => (
             <SwiperSlide key={info.id}>
               <div className={styles.slideScreen}>
-                <Image src={info.src} width={1370} height={700} alt={info.alt} priority />
+                <Image src={info.src} width={1000} height={700} alt={info.alt} priority />
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
+        {/* <div className={styles.actionButton}>
+          <PrevArrow className={styles.arrow} width={80} onClick={goPrev} />
+          <NextArrow className={styles.arrow} width={80} onClick={goNext} />
+        </div> */}
+      </div>
+      {/* 현재 활성화된 슬라이드에 해당하는 설명만 보여줌 */}
+      <div className={styles.description}>
+        <div className={styles.descriptionItem}>
+          <h1>{ADMIN_INFO[activeIndex].id}</h1>
+          <h2>{ADMIN_INFO[activeIndex].title}</h2>
+          <div className={styles.textBox}>
+            <p>{ADMIN_INFO[activeIndex].desc}</p>
+            <p>{ADMIN_INFO[activeIndex].caption}</p>
+          </div>
+        </div>
         <div className={styles.actionButton}>
           <PrevArrow className={styles.arrow} width={80} onClick={goPrev} />
           <NextArrow className={styles.arrow} width={80} onClick={goNext} />
--- src/data/scroll-props.ts
+++ src/data/scroll-props.ts
@@ -20,8 +20,36 @@ export const IPHONES = [
   { id: 3, src: 'images/iphone-shot/select.svg', alt: 'select' },
 ];
 export const ADMIN_INFO = [
-  { id: 1, src: 'images/admin-shot/admin-prod.svg', alt: 'admin-prod' },
-  { id: 2, src: 'images/admin-shot/admin-order-check.svg', alt: 'admin-order-check' },
-  { id: 3, src: 'images/admin-shot/admin-menu.svg', alt: 'admin-menu' },
-  { id: 4, src: 'images/admin-shot/admin-menu-option.svg', alt: 'admin-menu-option' },
+  {
+    id: 1,
+    src: 'images/admin-shot/admin-prod.svg',
+    alt: 'admin-prod',
+    title: '한 눈에 들어오는 직관적인 UI',
+    desc: '우리 매장에 들어온 주문을 한 화면에서 전부 관리할 수 있어요.',
+    caption: '매장 주문과 포장 주문을 직관적으로 나타내 빠르게 주문을 파악할 수 있답니다.',
+  },
+  {
+    id: 2,
+    src: 'images/admin-shot/admin-order-check.svg',
+    alt: 'admin-order-check',
+    title: '누가 먼저 주문하셨어요?',
+    desc: '바쁜 매장, 어떤 주문을 먼저 처리해야 할까요?',
+    caption: '매직포스에서 시간 순으로 나열된 주문내역을 보고 빠르게 주문을 관리해 보세요.',
+  },
+  {
+    id: 3,
+    src: 'images/admin-shot/admin-menu.svg',
+    alt: 'admin-menu',
+    title: '매직포스는 눈 보다 빠르지',
+    desc: '메뉴의 이름과 가격 설정은 물론, 수량과 다양한 옵션까지 추가해 보세요.',
+    caption: '필요할 때, 언제든 쉽게 수정할 수 있어요.',
+  },
+  {
+    id: 4,
+    src: 'images/admin-shot/admin-menu-option.svg',
+    alt: 'admin-menu-option',
+    title: '따듯한 아이스 아메리카노는 이제 안녕',
+    desc: '메뉴의 옵션을 다양하게 설정할 수 있어요.',
+    caption: '헷갈리는 주문을 되물어볼 필요도 없고, 주문을 실수할 일도 없어지죠.',
+  },
 ];
