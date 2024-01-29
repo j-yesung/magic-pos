3354df0abe35880fbea690f3dd123fe832023c84
LeeDongTak <sunny5796@naver.com>
2024-01-29 17:40:57 +0900

    🩹 fix: 각 페이지에 head테그 삽입

--- src/pages/admin/store/index.tsx
+++ src/pages/admin/store/index.tsx
@@ -1,15 +1,18 @@
 import AdminLayout from '@/components/layout/admin/AdminLayout';
 import StoreContents from '@/components/store/StoreMain';
+import { makeTitle } from '@/shared/helper';
 import Head from 'next/head';
 import { ReactNode } from 'react';
-import { makeTitle } from '@/shared/helper';
 
 const StorePage = () => {
   return (
     <>
       <Head>
         <title>{makeTitle('가게 관리')}</title>
-        <meta name="description" content="store" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="매직 포스" />
+        <meta name="keywords" content="magic pos,매직 포스, 키오스크, 가게관리 가게 설정" />
+        <meta name="description" content="magic pos의 가게 관리 페이지 입니다. " />
       </Head>
       <StoreContents />
     </>
--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,13 +1,16 @@
 import User from '@/components/auth/User';
-import Head from 'next/head';
 import { makeTitle } from '@/shared/helper';
+import Head from 'next/head';
 
 const SignupPage = () => {
   return (
     <>
       <Head>
         <title>{makeTitle('회원가입')}</title>
-        <meta name="description" content="signup" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="매직 포스" />
+        <meta name="keywords" content="magic pos,매직 포스, 키오스크, 회원가입" />
+        <meta name="description" content="magic pos의 회원가입 페이지 입니다. " />
       </Head>
       <User />
     </>
