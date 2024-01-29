92afc4aa234b034d27af02728d0c3927406bc750
j-yesung <hisung-ah@kakao.com>
2024-01-29 12:34:05 +0900

    🩹 fix: 문의사항 보내고 모달 닫기 (#271)

--- src/components/layout/admin/nav/SendMail.tsx
+++ src/components/layout/admin/nav/SendMail.tsx
@@ -3,7 +3,7 @@ import { useInput } from '@/hooks/service/auth/useInput';
 import { useModal } from '@/hooks/service/ui/useModal';
 import { useSendMail } from '@/hooks/service/useSendEmail';
 import useAuthState from '@/shared/store/session';
-import { useRef } from 'react';
+import { ChangeEvent, useRef } from 'react';
 import { IoClose } from 'react-icons/io5';
 import styles from '../styles/SendMail.module.css';
 
@@ -15,6 +15,11 @@ const SendMail = ({ modalId }: { modalId?: string }) => {
   const { value, changeHandler } = useInput({ user_email: '' });
 
   const clickCloseHandler = () => MagicModal.hide(modalId ?? '');
+  const sendEmailAndCloseModal = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sendEmail(e);
+    clickCloseHandler();
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -22,7 +27,7 @@ const SendMail = ({ modalId }: { modalId?: string }) => {
         <h1>문의하기</h1>
         <IoClose className={styles.ioClose} onClick={clickCloseHandler} />
       </div>
-      <form className={styles.formBox} ref={form} onSubmit={sendEmail}>
+      <form className={styles.formBox} ref={form} onSubmit={sendEmailAndCloseModal}>
         <div className={styles.inputBox}>
           <label htmlFor="to_name">상호명</label>
           <input type="text" id="to_name" name="to_name" value={storeName ?? ''} disabled />
