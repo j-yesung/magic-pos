395c04e2468209905435a7e9f266af7b7ca47d73
j-yesung <hisung-ah@kakao.com>
2024-01-31 16:59:59 +0900

    🩹 fix: link 상대 경로로 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,21 +5,21 @@ export default function Document() {
     <Html lang="ko">
       <Head />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" sizes="57x57" href="icons/favicon/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="icons/favicon/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="icons/favicon/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="icons/favicon/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="icons/favicon/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="icons/favicon/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="icons/favicon/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="icons/favicon/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="icons/favicon/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="icons/favicon/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="icons/favicon/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/icons/favicon/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/icons/favicon/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/icons/favicon/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/icons/favicon/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/icons/favicon/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/icons/favicon/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/icons/favicon/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/icons/favicon/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/favicon/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/icons/favicon/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon/favicon-16x16.png" />
       <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="icons/favicon/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/icons/favicon/ms-icon-144x144.png" />
       <meta name="theme-color" content="#ffffff" />
       <link
         href="splashscreens/iphone5_splash.png"
