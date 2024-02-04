f679439be1474a68b15d08549749f4b2739e4e22
lunaxislu <cmk0905@naver.com>
2024-02-04 13:39:29 +0900

    ✏️  '메'를 '매'로 바꿈

--- src/pages/admin/sales/index.tsx
+++ src/pages/admin/sales/index.tsx
@@ -35,8 +35,8 @@ const SalesPage = ({ holidays }: { holidays: HolidaysType }) => {
         <title>{makeTitle('매출 관리')}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="author" content="매직 포스" />
-        <meta name="keywords" content="magic pos,매직 포스, 키오스크, 메출관리, 달력으로메출 관리, 차트로 메출관리" />
-        <meta name="description" content="magic pos의 메출 관리 페이지 입니다. " />
+        <meta name="keywords" content="magic pos,매직 포스, 키오스크, 매출관리, 달력으로매출 관리, 차트로 매출관리" />
+        <meta name="description" content="magic pos의 매출 관리 페이지 입니다. " />
       </Head>
       <Sales />
     </>
