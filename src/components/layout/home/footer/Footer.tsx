cf3b1aa8a19d9123ad9066314794396adff8e2fc
j-yesung <hisung-ah@kakao.com>
2024-02-06 19:40:36 +0900

    🩹 fix: update contact us email

--- src/components/layout/home/footer/Footer.tsx
+++ src/components/layout/home/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className={styles.footerWrapper}>
       <Image src="/logo.svg" alt="logo" width={200} height={30} />
       <div className={styles.info}>
-        <p>Contact Us: jangyesung7@google.com</p>
+        <p>Contact Us: jangyesung7@gmail.com</p>
         <div className={styles.textArea}>
           <p>COPYRIGHT © 2024 MAGICPOS. ALL RIGHTS RESERVED.</p>
           <p className={styles.bold}>DESIGN BY YEJIN</p>
